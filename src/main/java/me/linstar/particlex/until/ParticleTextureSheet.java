89ae4707c1553833e075c3dc33a6fdebeabc0911
Hail122 <75189571+linstar-fxt@users.noreply.github.com>
2023-06-21 16:40:41 +0800

    Add files via upload

---
+++ src/main/java/me/linstar/particlex/until/ParticleTextureSheet.java
@@ -0,0 +1,7 @@
+package me.linstar.particlex.until;
+
+public class ParticleTextureSheet {
+    public static int PARTICLE_SHEET_OPAQUE = 0;
+    public static int PARTICLE_SHEET_TRANSLUCENT = 1;
+    public static int PARTICLE_SHEET_LIT = 2;
+}
