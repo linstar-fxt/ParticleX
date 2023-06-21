ebd239addd39dc35ada6f7fcb323aca69c43e051
Hail122 <75189571+linstar-fxt@users.noreply.github.com>
2023-06-21 16:44:19 +0800

    Update ParticleModify.java

--- src/main/java/me/linstar/particlex/until/ParticleModify.java
+++ src/main/java/me/linstar/particlex/until/ParticleModify.java
@@ -1,11 +1,9 @@
 package me.linstar.particlex.until;
-
-import net.minecraft.client.particle.ParticleTextureSheet;
 //自定义粒子属性，会随着版本增加
 
 public class ParticleModify {
     public boolean CHANGE_COLOR = false;
     public boolean BLOCK_RENDER = false;
-    public ParticleTextureSheet RENDER_TYPE = ParticleTextureSheet.NO_RENDER;
+    public int RENDER_TYPE = ParticleTextureSheet.PARTICLE_SHEET_LIT;
     public int LIGHT = -1;
 }
